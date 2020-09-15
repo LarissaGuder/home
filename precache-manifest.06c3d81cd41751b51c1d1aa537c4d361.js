2f1ea66f1ca24a3e2c3be6892550b449485dcb3f
larissaguder <lariguder10@gmail.com>
2020-09-14 23:35:54 -0300

    Updates

---
+++ precache-manifest.06c3d81cd41751b51c1d1aa537c4d361.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4c98a9cf53399aa86b1435d1bd9bde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0168dcd9e0cf5262508f",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5772d53b3c0b9fb66464",
+    "url": "/static/css/main.9fc689ea.chunk.css"
+  },
+  {
+    "revision": "0168dcd9e0cf5262508f",
+    "url": "/static/js/2.3f771261.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.3f771261.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5772d53b3c0b9fb66464",
+    "url": "/static/js/main.f3afd8bb.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/resume.d41d8cd9.pdf"
+  }
+]);
